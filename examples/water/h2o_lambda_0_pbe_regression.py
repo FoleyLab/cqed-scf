need to fix dispersion corrections for gradient

examples/water/h2o_lambda_0_pbe_regression.py
@@ -42,17 +42,17 @@ calc = CQEDSCF(
     psi4_options=psi4_options,
     omega=0.1,
     density_fitting=True,
-    functional="wB97x-D",
+    functional="wB97x",
     debug=True,
 )
 
-print("\nRunning CQED-B3LYP (λ = 0)\n")
+print("\nRunning wB97x-D (λ = 0)\n")
 
 E_qed, scf_data = calc.run()
 
-#gradient_engine = CQEDRHFGradient(lambda_vector, canonical="psi4", debug=False)
+gradient_engine = CQEDRHFGradient(lambda_vector, canonical="psi4", debug=False)
 
-#grad_qed = gradient_engine.compute(scf_data)
+grad_qed = gradient_engine.compute(scf_data)
 
 
 # ---------------------------------------------------------
@@ -63,12 +63,23 @@ psi4.set_options(psi4_options)
 
 mol = psi4.geometry(geometry)
 
-print("\nRunning Psi4 reference B3LYP\n")
+print("\nRunning Psi4 reference wB97x-D\n")
 
 E_ref, wfn_ref = psi4.energy("wB97X-D", return_wfn=True)
+E_no_disp, wfn_no_disp = psi4.energy("wB97X", return_wfn=True)
 
-#grad_ref = psi4.gradient("CAM", ref_wfn=wfn_ref).np
+try:
+   E_disp = psi4.variable("DISPERSION CORRECTION ENERGY")
+except KeyError:
+   E_disp = 0.0
+# subtract dispersion correction
+#E_ref -= E_disp
 
+grad_ref = psi4.gradient("wB97X-D", ref_wfn=wfn_ref).np
+
+grad_no_disp = psi4.gradient("wB97X", ref_wfn=wfn_no_disp).np
+
+disp_grad = grad_ref - grad_no_disp
 
 # ---------------------------------------------------------
 # Compare results
@@ -78,8 +89,8 @@ print("\n==============================")
 print(" Energy Comparison")
 print("==============================")
 
-print(f"CQED-B3LYP energy : {E_qed:20.12f}")
-print(f"Psi4 B3LYP energy : {E_ref:20.12f}")
+print(f"CQED-wB97X-D              energy : {E_qed:20.12f}")
+print(f"Psi4 wB97X-D - Dispersion energy : {E_ref:20.12f}")
 
 dE = abs(E_qed - E_ref)
 
@@ -91,22 +102,25 @@ print(" Gradient Comparison")
 print("==============================")
 
 #grad_qed = np.zeros((3,3))
-#grad_qed = np.array(grad_qed)
-#grad_ref = np.array(grad_ref)
+grad_qed = np.array(grad_qed) + disp_grad
+grad_ref = np.array(grad_ref)
 
-#grad_diff = grad_qed - grad_ref
+grad_diff = grad_qed - grad_ref
 
-#rms = np.sqrt(np.mean(grad_diff**2))
-#max_err = np.max(np.abs(grad_diff))
+rms = np.sqrt(np.mean(grad_diff**2))
+max_err = np.max(np.abs(grad_diff))
 
-#print(f"Gradient RMS error : {rms:12.6e}")
-#print(f"Gradient max error : {max_err:12.6e}")
+print(f"Gradient RMS error : {rms:12.6e}")
+print(f"Gradient max error : {max_err:12.6e}")
 
-#print("\nCQED gradient:")
-#print(grad_qed)
+print("\nCQED gradient:")
+print(grad_qed)
 
-#print("\nPsi4 gradient:")
-#print(grad_ref)
+print("\nPsi4 gradient:")
+print(grad_ref)
+
+print("\nDispersion gradient:")
+print(disp_grad)
 
 
 # ---------------------------------------------------------
@@ -120,7 +134,7 @@ print("\n==============================")
 print(" Regression Result")
 print("==============================")
 
-if dE < E_tol: # and rms < G_tol:
-    print("PASS: CQED-DFT reproduces Psi4 CAM.")
+if dE < E_tol and rms < G_tol:
+    print("PASS: CQED-DFT reproduces Psi4 wB97X-D.")
 else:
     print("FAIL: Results differ from Psi4 reference.")

src/cqed_rhf/scf.py
@@ -129,6 +129,16 @@ class CQEDSCF:
         ref_method = self._reference_method_string()
         _, self.wfn = psi4.energy(ref_method, return_wfn=True)
 
+        # compute the dispersion correction if using DFT
+        E_disp = 0.0
+        if self.is_dft:
+            try:
+                E_disp = self.wfn.variable("DISPERSION CORRECTION ENERGY")
+            except KeyError:
+                E_disp = 0.0
+    
+
+
         self.mints = psi4.core.MintsHelper(self.wfn.basisset())
         self.nbf = self.wfn.nmo()
         self.ndocc = self.wfn.nalpha()
@@ -319,8 +329,11 @@ class CQEDSCF:
         d_nuc = np.dot(self.lambda_vector, mu_nuc)
         d_exp = d_exp_el + d_nuc
 
+        E_total = E + E_disp
         results = dict(
-            energy=E,
+            energy_scf=E,
+            energy_dispersion=E_disp,
+            energy_total=E_total,
             density=D,
             coefficients=C,
             orbital_energies=eps,
@@ -339,7 +352,7 @@ class CQEDSCF:
             x_alpha=self.x_alpha,
         )
 
-        return E, results
+        return E_total, results
 
     # -------------------------
     # internal helpers