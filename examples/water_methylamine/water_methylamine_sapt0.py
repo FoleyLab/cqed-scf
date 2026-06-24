ede7d485e51c75be88734317b5c6ed46415ad83c
FoleyLab <jayfoleyiv@gmail.com>
2026-06-24 06:26:16 -0400

    added sapt0 example

---
+++ examples/water_methylamine/water_methylamine_sapt0.py
@@ -0,0 +1,92 @@
+import numpy as np
+import psi4
+
+from cqed_scf import CQEDConfig
+from cqed_scf.sapt import QEDSAPT0Driver
+
+#psi4.core.be_quiet()  # Suppress Psi4 output for cleaner test output
+
+
+psi4_options = {
+    "basis": "jun-cc-pVDZ",
+    "scf_type": "pk",
+    "e_convergence": 1e-12,
+    "d_convergence": 1e-12,
+}
+
+
+psi4.set_options(psi4_options)
+config = CQEDConfig(
+    lambda_vector=np.array([0.0, 0.0, 0.0]),
+    omega=0.0,
+    psi4_options=psi4_options,
+    reference="rhf",
+    functional=None,
+    density_fitting=False,
+    charge=0,
+    multiplicity=1,
+    dispersion_policy="none",
+    debug=False,
+)
+
+dimer = """
+0 1
+O   -0.687464896  -0.111744327  -0.019625472
+H   -1.046121544   0.775938208   0.012706845
+H    0.274042519   0.025850654  -0.003497262
+--
+0 1
+N    2.787113199   0.125007400   0.008492726
+H    3.082477630  -0.427630575  -0.786298137
+H    3.097193694  -0.385713691   0.825352219
+C    3.446448476   1.433371365  -0.031748912
+H    3.135906054   2.015096325   0.832766508
+H    4.537757766   1.394076393  -0.040704580
+H    3.119736204   1.969288834  -0.919572724
+symmetry c1
+no_com
+no_reorient
+"""
+
+
+dimer_geometry = psi4.geometry(dimer)
+sapt_driver = QEDSAPT0Driver(
+    dimer_geometry=dimer_geometry,
+    config=config,
+    integral_backend="full_eri",
+    include_cavity_terms=True,
+)
+
+SAPT0_Energy = sapt_driver.run()
+
+
+# Define a width for the labels to ensure perfect alignment
+w = 22
+
+print(f"{'Electrostatics:':<{w}} {sapt_driver.Eelst100:15.10f} Hartree")
+print(f"{'Exchange:':<{w}} {sapt_driver.Eexch100:15.10f} Hartree")
+print(f"{'Dispersion:':<{w}} {sapt_driver.Edisp200:15.10f} Hartree")
+print(f"{'Exchange-Dispersion:':<{w}} {sapt_driver.Eexchdisp200:15.10f} Hartree")
+print(f"{'Induction:':<{w}} {sapt_driver.Eind200:15.10f} Hartree")
+print(f"{'Exchange-Induction:':<{w}} {sapt_driver.Eexchind200:15.10f} Hartree")
+print("-" * 50)  # Visual separator for the total
+print(f"{'Total QED-SAPT0 Energy:':<{w}} {SAPT0_Energy:15.10f} Hartree")
+
+print()
+
+psi4.activate(dimer_geometry)
+e_sapt = psi4.energy("sapt0")
+
+# Access specific components
+elst = psi4.variable('SAPT ELST ENERGY')
+exch = psi4.variable('SAPT EXCH ENERGY')
+ind  = psi4.variable('SAPT IND ENERGY')
+disp = psi4.variable('SAPT DISP ENERGY')
+#total_sapt0 = psi4.variable('SAPT0 ENERGY')
+
+print(f"Electrostatics: {elst: .8f} Hartrees")
+print(f"Exchange:       {exch: .8f} Hartrees")
+print(f"Induction:      {ind: .8f} Hartrees")
+print(f"Dispersion:     {disp: .8f} Hartrees")
+print(f"Total SAPT0:    {e_sapt: .8f} Hartrees")
+
